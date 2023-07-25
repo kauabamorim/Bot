ca1f4f568fc9d2c98730a0c6f3d2addae74e8d4a
Kauã Berman Amorim <kauabamorim@outlook.com>
2023-07-25 16:50:15 -0300

    Get Multas

--- src/process.ts
+++ src/process.ts
@@ -2,21 +2,46 @@ import axios from "axios";
 
 export const botDetrans = async () => {
   let token = "";
-
+  
   try {
-    
     const response = await axios.post(
       "https://servicos.dnit.gov.br/auth-sior/renavam",
       {
-          placa: "OZG7778",
+        placa: "OZG7778",
         renavam: "1011222229",
       }
     );
 
     token = response.data.token;
-    console.log(token);
 
   } catch (error) {
     console.log(error);
+  }
+
+  try {
+    const response = await axios.get(
+      "https://servicos.dnit.gov.br/services-sior/portal-multas/infracoes",
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const infractions = response.data.infracoes;
+    const firstInfraction = infractions[0];
+
+    console.log("Auto de Infração:", firstInfraction.numeroAuto);
+    console.log("Descrição:", firstInfraction.enquadramento);
+    console.log("Veículo:", firstInfraction.veiculoPlacaUF);
+    console.log("Situação:", firstInfraction.situacaoFase);
+    console.log("Data e Hora:", firstInfraction.dataHora);
+    console.log("Amparo e Gravidade:", firstInfraction.gravidade);
+    console.log("Local:", firstInfraction.local);
+    console.log("Município:", firstInfraction.municipio);
+    console.log("Valor Original:", firstInfraction.valorMultaOriginal);
+
+  } catch (error) {
+    console.error(error);
   }
 };
